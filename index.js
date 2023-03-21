558fc5d46745277bf1f2e4642cc8797875926cc3
SrHatcher <erickomaldonado11@gmail.com>
2023-03-21 12:11:50 -0600

    added some commented code to change the slide animation to a fade animation

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const carrousel = document.querySelector('.carrousel_container')
 const leftButtons = document.querySelectorAll('.testimonial__left_button')
 const rightButtons = document.querySelectorAll('.testimonial__right_button')
 
+//To change the animation for changing the testimonial, uncomment the the commented code
+//const testimonials = document.querySelectorAll('.main__testimonial')
+//testimonials[1].style.display = 'none'
+
 leftButtons.forEach((button, index)=>{
     button.addEventListener('click', ()=>{
         if(!index){
             return;
         }else{
+            //to change the animation uncomment the next  two lines
+            // testimonials[index].style.display = 'none'
+            // testimonials[index - 1].style.display = 'flex'
+
+            //to change the animation comment the next line
             carrousel.style.transform = 'translateX(0%)'
+
         }
     })
 })
@@ -17,6 +27,11 @@ leftButtons.forEach((button, index)=>{
 rightButtons.forEach((button, index)=>{
     button.addEventListener('click', ()=>{
         if(!index){
+            //to change the animation uncomment the next  two lines
+            // testimonials[index].style.display = 'none'
+            // testimonials[index + 1].style.display = 'flex'
+
+            //to change the animation comment the next line
             carrousel.style.transform = 'translateX(-51.5%)'
         }else{
             return;
